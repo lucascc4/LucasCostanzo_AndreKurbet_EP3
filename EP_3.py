74e1537d83d955dfd4b374002bf7faabc195f2ac
lucascc4 <lucascc4@al.insper.edu.br>
2015-04-23 22:49:17 -0300

    commit 8
    programa finalizado

--- EP_3.py
+++ EP_3.py
@@ -9,9 +9,9 @@ from funcoes import *
 import matplotlib.pyplot as plt
 import datetime
 
-x = open("alimentos.csv", encoding="latin1")
+x = open("alimentos.csv", encoding="latin1") # abre a lista de alimentos
 x = x.readlines()
-y = open("usuario.csv", encoding ="latin1")
+y = open("usuario.csv", encoding ="latin1")# abre as informaçoes do usuario
 y = y.readlines()
 tabela = {} 
 pessoa = {}
@@ -19,13 +19,13 @@ comidas = {}
 
 
 
-for i in range (1,len(x)):
+for i in range (1,len(x)):  #transforma lista de alimentos em um dicionario
     x[i] = x[i].strip()
     a = x[i].split(",")
     tabela[a[0]] = [a[1],a[2],a[3],a[4],a[5]]
 
 
-for i in range (len(y)):
+for i in range (len(y)): #tranforma as inforçoes do usuario em dois dicionarios, um com os dados pessoais e o outro com os alimentos e datas
     y[i] = y[i].strip()
     linha = y[i].split(",")
     if i == 1:
@@ -53,7 +53,7 @@ listapro = []
 listagor = []
 listacar = []
             
-for data in sorted(comidas):
+for data in sorted(comidas): #calcula as calorias, proteinas, gorduras e carboidrtos e os coloca em listas separadas, intercalando-os com suas resoectivas datas
     listacal.append(data)
     listapro.append(data)
     listagor.append(data)
@@ -121,7 +121,7 @@ for i in range(len(listacar)-1):
 
 
 
-calorias_diarias = 0
+calorias_diarias = 0 # calcula as calorias ideais consumidas por dia
 if pessoa[3] == "M":
     calorias_diarias_homem = CalculaHBhomem(float(pessoa[2]),float(pessoa[4]),float(pessoa[1]))
     calorias_diarias += calorias_diarias_homem
@@ -131,7 +131,7 @@ else:
 #rint(calorias_diarias)  
 
 
-TMB = TMB(calorias_diarias,pessoa[5])  
+TMB = TMB(calorias_diarias,pessoa[5])  # multiplica as calorias ideais pelo fator de execicios
 
   
     
@@ -142,20 +142,20 @@ carboidratos_plot = []
 dias = []
 datas = []
 
-for i in range (len(listacal)):
+for i in range (len(listacal)): #adiciona os dias em uma lista
     if i%2 == 0:   
         dias.append(listacal[i])
 
 
 for i in dias:
-    d = datetime.datetime.strptime(i, "%d/%m/%Y").strftime("%d/%m/%Y")
+    d = datetime.datetime.strptime(i, "%d/%m/%Y").strftime("%d/%m/%Y")#acerta as datas
     datas.append(d)
 
-dias_ordenados = sorted(datas)
+dias_ordenados = sorted(datas)# colocoa as datas em ordem
 x = list(range(len(dias_ordenados)))
 y = []
 
-for i in dias_ordenados:
+for i in dias_ordenados: #coloca as calorias na mesma ordem das datas
     if i in listacal_dict:
         calorias_plot.append(listacal_dict[i])
 #print(calorias_plot)
@@ -172,17 +172,26 @@ for i in dias_ordenados:
 for i in dias_ordenados:
     if i in listacar_dict:
         carboidratos_plot.append(listacar_dict[i])
-            
+
+
+calorias_ideais_plot = []
+for i in range(len(x)):
+    calorias_ideais_plot.append(TMB)
         
 
-plt.plot(x,calorias_plot)
+plt.plot(x,calorias_plot) #cria o grafico
+plt.plot(x,calorias_ideais_plot)
 plt.ylabel("Calorias")
 plt.xlabel("Dias")
 plt.title(r' Calorias')
+plt.plot(x,calorias_plot,"r", label = "calorias consumidas")
+plt.plot(x,calorias_ideais_plot,"g", label = "calorias consumidas")
+plt.legend(loc = "center right")
 plt.show()
 
 
 plt.plot(x,proteinas_plot)
+plt.plot(x,proteinas_plot,"r", label = "calorias consumidas")
 plt.ylabel("Proteinas")
 plt.xlabel("Dias")
 plt.title(r' Proteinas')
@@ -190,6 +199,7 @@ plt.show()
 
 
 plt.plot(x,gorduras_plot)
+plt.plot(x,gorduras_plot,"r", label = "calorias consumidas")
 plt.ylabel("Gorduras")
 plt.xlabel("Dias")
 plt.title(r' Gorduras')
@@ -197,6 +207,7 @@ plt.show()
 
 
 plt.plot(x,carboidratos_plot)
+plt.plot(x,carboidratos_plot,"r", label = "calorias consumidas")
 plt.ylabel("Carboidratos")
 plt.xlabel("Dias")
 plt.title(r' Carboidratos')
